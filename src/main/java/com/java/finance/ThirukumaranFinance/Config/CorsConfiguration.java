9d51fab7c2dfefa1317c4649d7389e7fe916944f
dharanikanagaraj <134994397+dharanikanagaraj@users.noreply.github.com>
2023-06-25 10:23:31 +0530

    updated cors config Url

--- src/main/java/com/java/finance/ThirukumaranFinance/Config/CorsConfiguration.java
+++ src/main/java/com/java/finance/ThirukumaranFinance/Config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://thirukumaran-finance.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
